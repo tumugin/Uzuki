e271628f4f5b11f8b50507f66cc136a36ac84b36
Kazuki Oishi <oishikazuki@gmail.com>
2016-02-14 19:56:20 +0900

    書き込みメソッドを実装し直し

---
+++ Uzuki/2ch/Write/Write2chThreadV2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using Uzuki.Network;
+
+namespace Uzuki._2ch.Write
+{
+    public class Write2chThreadV2
+    {
+        private const String WRITE_USER_AGENT = "Uzuki/1.0";
+        private static Encoding WriteEncoding = Encoding.GetEncoding("Shift-JIS");
+        private const String WRITE = "書き込む";
+        private String ThreadBBSName;
+        private String ThreadBBSKey;
+        private String ThreadBBSHost;
+        private CookieContainer cookiecontainer;
+
+        public Write2chThreadV2(String bbs,String key,String host,CookieContainer ck)
+        {
+            ThreadBBSName = bbs;
+            ThreadBBSKey = key;
+            ThreadBBSHost = host;
+            cookiecontainer = ck;
+        }
+
+        public void Write(String name,String mail,String message)
+        {
+            //make write params
+            String req = string.Concat(new string[]
+            {
+                "bbs=",
+                ThreadBBSName,
+                "&key=",
+                ThreadBBSKey,
+                "&time=",Tools.UnixTime.ToUnixTime(DateTime.Now).ToString(),
+                "&submit=",
+                HttpUtility.UrlEncode(WRITE, WriteEncoding),
+                "&FROM=",
+                HttpUtility.UrlEncode(name, WriteEncoding),
+                "&mail=",
+                HttpUtility.UrlEncode(mail, WriteEncoding),
+                "&MESSAGE=",
+                HttpUtility.UrlEncode(message, WriteEncoding)
+            });
+            byte[] encoded = Encoding.ASCII.GetBytes(req);
+            //generate referer
+            String referer = "http://" + ThreadBBSHost + "/" + ThreadBBSKey;
+            //make request
+            HttpWebRequest httpreq = (HttpWebRequest)WebRequest.Create("http://" + ThreadBBSHost + "/test/bbs.cgi?guid=ON");
+            httpreq.Method = "POST";
+            httpreq.ContentType = "application/x-www-form-urlencoded";
+            httpreq.CookieContainer = cookiecontainer;
+            httpreq.Referer = referer;
+            httpreq.UserAgent = WRITE_USER_AGENT;
+            httpreq.ContentLength = encoded.LongLength;
+            //write to request stream
+            Stream st = httpreq.GetRequestStream();
+            st.Write(encoded, 0, encoded.Length);
+            st.Close();
+            //get return value
+            HttpWebResponse response = (HttpWebResponse)httpreq.GetResponse();
+            //copy stream
+            MemoryStream mems = Ayane.CopyAndClose(response.GetResponseStream());
+            byte[] context = mems.ToArray();
+            mems.Close();
+            //get encoding
+            Encoding encode = Ayaka.GetCode(context);
+            String html = encode.GetString(context);
+            Debug.WriteLine(html);
+            //以下エラー判定
+            if (!html.Contains("書きこみました"))
+            {
+                //書き込めていない
+                //エラー文字列をHTMLからテキストに変換する
+                Regex re = new Regex("<.*?>", RegexOptions.Singleline);
+                Regex br = new Regex("<br>", RegexOptions.Singleline);
+                string output = br.Replace(html, Environment.NewLine);
+                output = re.Replace(output, "");
+                throw new Exception(output);
+            }
+            
+        }
+
+        //Exception
+        public class Write2chWriteException : Exception { }
+    }
+}
---
+++ UzukiTest/WriterTester.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uzuki;
+using Uzuki._2ch.Write;
+using System.Net;
+using System.Diagnostics;
+
+namespace UzukiTest
+{
+    [TestClass]
+    public class WriterTester
+    {
+        [TestMethod]
+        public void WriteTest()
+        {
+            CookieContainer ck = new CookieContainer();
+            Write2chThreadV2 write = new Write2chThreadV2("ghard", "1455342643", "wktk.2ch.net", ck);
+            try
+            {
+                write.Write("","sage","てすと");
+            }
+            catch (Exception ex)
+            {
+                //書き込みエラーメッセージは例外をスローする
+                Debug.WriteLine(ex.Message);
+            }
+            
+            //2回目送信すると認証を通過できる
+            write.Write("", "sage", "てすと");
+        }
+
+        [TestMethod]
+        public void WriteOldTest()
+        {
+            CookieContainer ck = new CookieContainer();
+            Writer writer = new Writer("http://sweet.2ch.sc/test/read.cgi/kawaii/1438245456/");
+            WriteResponse wr =  writer.Write("", "sage", "てすとてすと");
+            wr.GetResult();
+            Debug.WriteLine(wr.Result);
+            WriteResponse wr2 = writer.Write("", "sage", "てすとてすと");
+            wr2.GetResult();
+            Debug.WriteLine(wr2.Result);
+        }
+    }
+}
